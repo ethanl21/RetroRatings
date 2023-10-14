6be1b871fda9fb1dcbbddec9dbe115f36e0c6164
Nalluh <120067040+Nalluh@users.noreply.github.com>
2023-10-14 16:53:24 -0700

    Delete cpsc349-project-main directory
    didnt mean to add entire folder mb

--- cpsc349-project-main/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/cpsc349-project/" // !!! change this if the repo name changes
-})
