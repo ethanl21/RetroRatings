d92da667db6eca36c00911658bfb72cc46ec34cc
jose-gonzalez1 <jose.gonz@csu.fullerton.edu>
2023-10-25 22:06:14 -0700

    Added tabs and modal for signing in.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import { AuthCard } from "./components/AuthCard";
 import { auth } from "./config/firebase";
 
 import Button from "react-bootstrap/Button";
-import Stack from "react-bootstrap/Stack";
+// import Stack from "react-bootstrap/Stack"; // Not used
+
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+
+import Modal from "react-bootstrap/Modal";
 
 function App() {
   const [authActionType, setAuthActionType] = useState<"signup" | "signin">(
@@ -19,34 +24,67 @@ function App() {
     }
   };
 
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
+
   return (
     <>
-      <AuthCard actionType={authActionType} />
+      <Tabs
+        defaultActiveKey="profile"
+        id="uncontrolled-tab-example"
+        className="mb-3"
+      >
+        <Tab eventKey="home" title="Home">
+          <>
+            <Button variant="primary" onClick={openModal}>
+              Sign In
+            </Button>
 
-      {/* debug stuff. this is not part of the Auth component */}
-      <Stack gap={3} direction="horizontal" className="mt-5">
-        <Button
-          onClick={() => {
-            authActionType === "signup"
-              ? setAuthActionType("signin")
-              : setAuthActionType("signup");
-          }}
-        >
-          Toggle Form Type
-        </Button>
-        <Button onClick={logoutUser}>Sign Out</Button>
-        <Button
-          onClick={() =>
-            alert(
-              auth.currentUser?.email
-                ? auth.currentUser?.email
-                : "You're not logged in!"
-            )
-          }
-        >
-          Who am I?
-        </Button>
-      </Stack>
+            <Modal show={showModal} onHide={closeModal}>
+              <Modal.Dialog>
+                <Modal.Header closeButton>
+                  <Modal.Title>Login Modal</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                  <AuthCard actionType={authActionType} />
+                </Modal.Body>
+
+                <Modal.Footer>
+                  <Button
+                    onClick={() => {
+                      authActionType === "signup"
+                        ? setAuthActionType("signin")
+                        : setAuthActionType("signup");
+                    }}
+                  >
+                    Toggle Form Type
+                  </Button>
+                  <Button onClick={logoutUser}>Sign Out</Button>
+                  <Button
+                    onClick={() =>
+                      alert(
+                        auth.currentUser?.email
+                          ? auth.currentUser?.email
+                          : "You're not logged in!"
+                      )
+                    }
+                  >
+                    Who am I?
+                  </Button>
+                  <Button variant="secondary" onClick={closeModal}>
+                    Close
+                  </Button>
+
+                </Modal.Footer>
+              </Modal.Dialog>
+            </Modal>
+          </>
+        </Tab>
+        <Tab eventKey="leaderboard" title="Leaderboard"></Tab>
+      </Tabs>
     </>
   );
 }
