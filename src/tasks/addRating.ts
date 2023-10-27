200332d37e0b626a9c211ab1cc75f05f0300254d
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-10-26 19:17:55 -0700

    Create addRating.ts
    Implement adding ratings

---
+++ src/tasks/addRating.ts
@@ -0,0 +1,23 @@
+import { auth, db } from "../config/firebase";
+import { doc, setDoc } from "firebase/firestore";
+
+export const addRating = async (ratingItemId: string, ratingValue: number) => {
+  try {
+    if (!auth.currentUser) {
+      return Promise.reject("Only logged in users can add ratings");
+    }
+
+    const userRatingsRef = doc(db, "user-ratings", auth.currentUser.uid);
+
+    const ratingMap = new Map();
+    ratingMap.set(ratingItemId, ratingValue);
+    const newRatingData = {
+      ratingValues: Object.fromEntries(ratingMap),
+    };
+
+    await setDoc(userRatingsRef, newRatingData, { merge: true });
+    return Promise.resolve();
+  } catch (err) {
+    console.log(err);
+  }
+};
