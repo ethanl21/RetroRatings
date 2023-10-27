46e0f3e1b5aa79cc500d5f92e7ebf774e65546dc
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-10-26 19:18:05 -0700

    Create getRatingItems.ts
    implement get items that can be rated

---
+++ src/tasks/getRatingItems.ts
@@ -0,0 +1,23 @@
+import { collection, getDocs, query, limit } from "firebase/firestore";
+import { auth, db } from "../config/firebase";
+
+export const getRatingItems = async (count: number) => {
+  try {
+    if(!auth.currentUser){
+        return Promise.reject("Only logged in users can view rating items.");
+    }
+
+    const q = query(collection(db, "rating-items"), limit(count));
+
+    const querySnapshot = await getDocs(q);
+    
+    const returnedRatingItems = new Map();
+    querySnapshot.forEach((doc) => {
+        returnedRatingItems.set(doc.id, doc.data());
+    })
+
+    return Promise.resolve(Object.fromEntries(returnedRatingItems));
+  } catch (err) {
+    console.log(err);
+  }
+};
