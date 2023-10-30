3951da79195bc172d211df6ca12c05e96fccae2c
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-10-29 18:00:32 -0700

    Modify style
    Show auth card if user isn't signed in, add some contrast

--- src/components/FirebaseImage.tsx
+++ src/components/FirebaseImage.tsx
@@ -1,5 +1,5 @@
 import { getDownloadURL, ref } from "firebase/storage";
-import { useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 import { ImageProps } from "react-bootstrap";
 import { storage } from "../config/firebase";
 import Image from "react-bootstrap/Image";
@@ -7,7 +7,7 @@ import Image from "react-bootstrap/Image";
 export const FirebaseImage = ({ ...props }: ImageProps) => {
   const [imageSrc, setImageSrc] = useState("");
 
-  useEffect(() => {
+  useMemo(() => {
     getDownloadURL(ref(storage, props.src)).then((val) => {
       setImageSrc(val);
     });
